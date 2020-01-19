02722c40c4d99635a7c4150232b197dc53c0cafe
Saurabh <codeclassifiers@gmail.com>
2020-01-19 10:07:42 +0530

    updated

---
+++ frontend/src/components/BookCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const BookCard = (props) => {
+    const  book  = props.book;
+
+    return(
+        <div className="card-container">
+            <img src="https://cdn.pixabay.com/photo/2015/06/02/12/59/narrative-794978_960_720.jpg" alt="" />
+            <div className="desc">
+                <h2>
+                    <Link to={`/show-book/${book._id}`}>
+                        { book.title }
+                    </Link>
+                </h2>
+                <h3>{book.author}</h3>
+                <p>{book.description}</p>
+            </div>
+        </div>
+    )
+};
+
+export default BookCard;
